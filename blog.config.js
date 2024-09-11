07374ce777e0c0d5f63d49afea7c35c711100ae9
zmy10032 <154607881+zmy10032@users.noreply.github.com>
2024-09-11 21:41:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -359,7 +359,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_COMMENT_UTTERRANCES_REPO || '', // 你的代码仓库名， 例如我是 'tangly1024/NotionNext'； 更多文档参考 https://utteranc.es/
 
   // giscus @see https://giscus.app/
-  COMMENT_GISCUS_REPO: process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO || 'zmy10032/NotionNext', // 你的Github仓库名 e.g 'tangly1024/NotionNext'
+  COMMENT_GISCUS_REPO: process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO || 'giscus/giscus', // 你的Github仓库名 e.g 'tangly1024/NotionNext'
   COMMENT_GISCUS_REPO_ID: process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO_ID || 'MDEwOlJlcG9zaXRvcnkzNTE5NTgwNTM=', // 你的Github Repo ID e.g ( 設定完 giscus 即可看到 )
   COMMENT_GISCUS_CATEGORY_ID:
     process.env.NEXT_PUBLIC_COMMENT_GISCUS_CATEGORY_ID || 'DIC_kwDOFPp0Jc4CcjOR', // 你的Github Discussions 內的 Category ID ( 設定完 giscus 即可看到 )
